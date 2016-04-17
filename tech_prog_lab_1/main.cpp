ac30685996b47d27a5221924430d163a3873df1b
radiationx <radiationx@yandex.ru>
2016-04-17 20:52:35 +0300

    add labs

--- tech_prog_lab_1/main.cpp
+++ tech_prog_lab_1/main.cpp
@@ -1,8 +1,130 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int mainSize,
+        tapeSize,
+        maxNumber = 90,
+        selectIndex = 0,
+        NULL_ELEMENT = -1;
+
+struct Element {
+    int numbers[3];
+};
+
+void printMainArray(Element *array);
+
+void printTapeArray(Element **array);
+
 int main() {
-    cout << "Hello, World!" << endl;
+    srand(time(NULL));
+
+    cout << "Введите количество элементов: ";
+    cin >> mainSize;
+
+    double sqrtMainSize = sqrt(mainSize);
+    //Размерность лент
+    tapeSize = (int) (sqrtMainSize - floor(sqrtMainSize) == 0 ? sqrtMainSize : sqrtMainSize + 1);
+
+    Element *mainArray = new Element[mainSize];
+    Element **tapeArray = new Element *[tapeSize];
+
+    for (int i = 0; i < mainSize; i++)
+        for (int j = 0; j < 3; ++j)
+            mainArray[i].numbers[j] = 10 + rand() % maxNumber;
+
+    cout << endl << "Изначальные данные:" << endl;
+    printMainArray(mainArray);
+    cout << endl;
+
+
+    //----Преобразование в ленты----//
+    for (int i = 0; i < tapeSize; i++)
+        tapeArray[i] = new Element[tapeSize];
+
+    for (int i = 0, m = 0; i < tapeSize; i++) {
+        for (int j = 0; j < tapeSize; j++, m++) {
+            tapeArray[i][j] = mainArray[m];
+            if (m >= mainSize)
+                for (int k = 0; k < 3; k++)
+                    tapeArray[i][j].numbers[k] = NULL_ELEMENT;
+        }
+    }
+
+    cout << "Преобразование в ленты:" << endl;
+    printTapeArray(tapeArray);
+    cout << endl;
+
+
+    //----Сортировка----//
+    Element element;
+    for (int i = 0; i < tapeSize; ++i)
+        for (int j = 0; j < tapeSize; ++j)
+            for (int k = 0; k < tapeSize; ++k)
+                if (tapeArray[i][j].numbers[selectIndex] < tapeArray[i][k].numbers[selectIndex]) {
+                    element = tapeArray[i][j];
+                    tapeArray[i][j] = tapeArray[i][k];
+                    tapeArray[i][k] = element;
+                }
+
+
+    cout << "Сортированные ленты:" << endl;
+    printTapeArray(tapeArray);
+    cout << endl;
+
+
+    //----Слияние лент----//
+    int minIndex = 0,
+            *index = new int[tapeSize];
+    int minNumber;
+
+    delete[](mainArray);
+    mainArray = new Element[mainSize];
+
+    for (int i = 0; i < tapeSize; index[i] = 0, i++);
+
+    for (int k = 0; k < mainSize; k++) {
+        minNumber = maxNumber + 10;
+        for (int i = 0; i < tapeSize; i++) {
+            if (index[i] < tapeSize && minNumber > tapeArray[i][index[i]].numbers[selectIndex]) {
+                minIndex = i;
+                minNumber = tapeArray[i][index[i]].numbers[selectIndex];
+            }
+        }
+        if (tapeArray[minIndex][index[minIndex]].numbers[selectIndex] == NULL_ELEMENT)
+            k--;
+
+        mainArray[k] = tapeArray[minIndex][index[minIndex]];
+        index[minIndex]++;
+    }
+
+    cout << "Слияние лент:" << endl;
+    printMainArray(mainArray);
     return 0;
+}
+
+void printMainArray(Element *array) {
+    for (int i = 0; i < mainSize; i++) {
+        cout << "{";
+        for (int j = 0; j < 3; ++j) {
+            cout << array[i].numbers[j];
+            if (j != 2) cout << " ";
+        }
+        cout << "}" << endl;
+    }
+}
+
+void printTapeArray(Element **array) {
+    for (int i = 0; i < tapeSize; i++) {
+        for (int j = 0; j < tapeSize; ++j) {
+            cout << "{";
+            for (int k = 0; k < 3; k++) {
+                cout << array[i][j].numbers[k];
+                if (k != 2) cout << " ";
+            }
+            cout << "} ";
+        }
+        cout << endl;
+    }
 }
--- tech_prog_lab_2/main.cpp
+++ tech_prog_lab_2/main.cpp
@@ -1,8 +1,214 @@
 #include <iostream>
+#include <math.h>
+
+#define L 5
 
 using namespace std;
+int N;
+
+struct generalStructure {
+    float key1;
+    float key2[L];
+    char key3;
+};
+struct floatStructure {
+    float key;
+    int index;
+};
+struct floatArrayStructure {
+    float key[L];
+    int index;
+};
+struct charStructure {
+    char key;
+    int index;
+};
+
+template<typename T>
+void sortSimple(T *array, int count);
+
+template<typename T>
+void sortArray(T *array, int count);
+
+template<typename T, typename A>
+int searchArray(const T *array, const A arg);
+
+template<typename T, typename A>
+int searchSimple(const T *array, const A arg);
+
 
 int main() {
-    cout << "Hello, World!" << endl;
+    srand(time(NULL));
+
+    cout << "Введите количество элементов: ";
+    cin >> N;
+    cout << endl;
+
+    generalStructure *general = new generalStructure[N];
+    floatStructure *catFloat = new floatStructure[N];
+    floatArrayStructure *catFloatArray = new floatArrayStructure[N];
+    charStructure *catChar = new charStructure[N];
+
+    int n = 0;
+    int *resultIndexes = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        general[i].key1 = floor(((90) * ((float) rand() / RAND_MAX)) + 10);
+        for (int j = 0; j < L; j++)
+            general[i].key2[j] = floor(((90) * ((float) rand() / RAND_MAX)) + 10);
+        general[i].key3 = 'a' + (char) (rand() % 26);
+
+        catFloat[i].key = general[i].key1;
+        catFloat[i].index = i;
+        for (int j = 0; j < L; j++)
+            catFloatArray[i].key[j] = general[i].key2[j];
+        catFloatArray[i].index = i;
+        catChar[i].key = general[i].key3;
+        catChar[i].index = i;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << "{" << general[i].key1 << ", [";
+        for (int j = 0; j < L; j++)
+            cout << general[i].key2[j] << " ";
+        cout << "], " << general[i].key3 << "}" << endl;
+    }
+
+    /*sortSimple(catFloat, N);
+    sortArray(catFloatArray, N);
+    sortSimple(catChar, N);*/
+
+    cout << endl;
+    float arg1;
+    cout << "Поиск в поле float: ";
+    cin >> arg1;
+    int resultFloat = searchSimple(catFloat, arg1);
+    if (resultFloat != -1) {
+        resultIndexes[n] = resultFloat;
+        n++;
+    }
+
+
+    cout << endl;
+    int arg2[L];
+    cout << "Поиск в поле int[" << L << "]: " << endl;
+    for (int k = 0; k < L; k++) {
+        cout << "Введите число " << k << ": ";
+        cin >> arg2[k];
+    }
+    int resultFloatArray = searchArray(catFloatArray, arg2);
+    if (resultFloatArray != -1) {
+        if (resultFloatArray != resultFloat) {
+            resultIndexes[n] = resultFloatArray;
+            n++;
+        }
+    }
+
+    cout << endl;
+    char arg3;
+    cout << "Поиск в поле char: ";
+    cin >> arg3;
+    int resultChar = searchSimple(catChar, arg3);
+    if (resultChar != -1) {
+        if (resultChar != resultFloat & resultChar != resultFloatArray) {
+            resultIndexes[n] = resultChar;
+            n++;
+        }
+    }
+
+    cout << endl;
+    if (n == 0)
+        cout << "Нет совпадений." << endl;
+    else {
+        cout << "Совпадения (" << n << "): " << endl;
+        for (int i = 0; i < n; i++) {
+            cout << "{" << general[resultIndexes[i]].key1 << ", [";
+            for (int j = 0; j < L; j++)
+                cout << general[resultIndexes[i]].key2[j] << " ";
+            cout << "], " << general[resultIndexes[i]].key3 << "}" << endl;
+        }
+    }
+
     return 0;
+}
+
+template<typename T>
+void sortSimple(T *array, int count) {
+    T temp;
+    for (int i = 0; i < count; ++i)
+        for (int j = i + 1; j < count; ++j)
+            if (array[j].key < array[i].key) {
+                temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+}
+
+template<typename T>
+void sortArray(T *array, int count) {
+    int temp;
+    for (int i = 0; i < count; i++)
+        for (int j = 0; j < L; j++)
+            for (int k = 0; k < L; k++)
+                if (array[i].key[j] < array[i].key[k]) {
+                    temp = array[i].key[j];
+                    array[i].key[j] = array[i].key[k];
+                    array[i].key[k] = temp;
+                }
+}
+
+template<typename T, typename A>
+int searchSimple(const T *array, const A arg) {
+    int resIndex = -1,
+            left = 1,
+            right = N,
+            middle;
+
+    while (((right - left) != 1) || (array[resIndex].key == arg)) {
+        middle = (left + right) / 2;
+        if (arg == (int) array[middle].key) {
+            resIndex = middle;
+            break;
+        }
+        if (left > 0)
+            left--;
+        else if (right > 0)
+            right--;
+    }
+    if (arg == (int) array[right].key)
+        resIndex = right;
+    if (arg == (int) array[left].key)
+        resIndex = left;
+    return resIndex;
+}
+
+template<typename T, typename A>
+int searchArray(const T *array, const A arg) {
+    int resIndex = -1,
+            left = 1,
+            right = N,
+            middle;
+    bool found = false;
+
+    while (right > left) {
+        middle = (right + left) / 2;
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < L; j++) {
+                if (arg[j] == array[middle].key[i]) {
+                    resIndex = middle;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        if (found)
+            break;
+        else if (left > 0)
+            left--;
+        else if (right > 0)
+            right--;
+        else break;
+    }
+    return resIndex;
 }
