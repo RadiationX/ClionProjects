69458a5a082451b7043a95fa0e092a4865ad7b03
radiationx <radiationx@yandex.ru>
2016-05-15 22:05:44 +0300

    it is extremely sad moment

--- tech_prog_lab3/LinkedList.h
+++ tech_prog_lab3/LinkedList.h
@@ -12,119 +12,40 @@ using namespace std;
 
 template<class T>
 class ListItem : T {
-private:
-    T data;
-    bool empty = true;
-public:
-    ListItem() { }
 
-    ListItem(T element) {
-        data = element;
-    }
 
-    T getData() {
-        return data;
-    }
 
-    void setData(T element) {
-        data = element;
-        empty = false;
-    }
 
-    bool isEmpty() {
-        return empty;
-    }
 
 };
 
 template<class T, int size>
 class Block {
 public:
-    int length = 0;
-    ListItem<T> *items;
     Block *nextBlock;
     Block *prevBlock;
-
-    Block() {
-        cout<<"CONSTRUCT MY PUSSY"<<endl;
-        items  = new ListItem<T>[size];
-    };
-
-
-
-    ListItem<T> get(int position) {
-        return items[position];
-    }
-
-    Block getLast() {
-        return items[length];
-    }
-
-    int getSize() {
-        return length;
-    }
-
-    void add(T element, int position) {
-        items[position].setData(element);
-        length++;
-    }
-    ~Block(){
-        //delete items[1];
-        //cout<<"DESTRUCT MY PUSSY"<<endl;
-    }
 };
 
 template<class T, int blockSize>
 class LinkedList {
 
-private:
-    int allocSize = 16;
-    int size = 0;
-    int blocksNumber = 0;
-
 
 
 public:
-    Block<T, blockSize> *array;
     LinkedList() {
-        array = new Block<T, blockSize>[allocSize];
+
     }
 
     void addBegin(T element) {
-        add(element, 0);
+
     }
 
     void add(T element) {
-        add(element, size);
+
     }
 
     void add(T element, int position) {
-        if(size>=allocSize||position>=allocSize){
-            cout<<"CALL REALLOC "<<size<<" : "<<allocSize<<endl;
-            int increment = allocSize*(position/allocSize);
-            reAlloc(increment);
-        }
-        int block = position / blockSize;
-        int item = position % blockSize;
-        bool needShift = (position < size | position > allocSize) & !array[block].items[item].isEmpty();
-        cout << "[begin add] to position [" << position << "] in [" << block << ", " << item << "]; size [" << size << "]; needShift {" << (needShift ? "true" : "false") << "}";
-        cout << "  (" << position << "<" << size << " || " << position << ">" << allocSize << ")&&" <<(!array[block].items[item].isEmpty() ? "true" : "false") << endl;
 
-
-        if (needShift) {
-            size++;
-        } else {
-            if (size <= position)
-                size = position + 1;
-        }
-
-
-        if (needShift)
-            shiftItems(block, item);
-
-        array[block].add(element, item);
-        cout << "[end   add] to position [" << position << "] in [" << block << ", " << item << "]; size [" << size <<
-        "]" << endl << endl;
     }
 
     void remove(int position) {
@@ -136,69 +57,8 @@ public:
     }
 
     void shiftItems(int fromBlock, int fromItem) {
-        cout << "call shift" << endl;
-        for (int i = size - 1, block, item; i > 0; i--) {
-            block = i / blockSize;
-            item = i % blockSize;
-            cout<<"shift "<<block<<" : "<<item<<endl;
-            if (item < fromItem) continue;
-            if (item == 0) {
-                array[block].items[item] = array[block - 1].items[blockSize - 1];
-            } else {
-                array[block].items[item] = array[block].items[item - 1];
-            }
-        }
+
     }
-
-    string toString() {
-        string res = "";
-
-        return res;
-    }
-
-
-    T get(int position) {
-        return array[position % blockSize].items[position].getData();
-    }
-
-    Block<T, blockSize> getBlock(int position) {
-        return array[position];
-    };
-
-    int getSize() {
-        return size;
-    }
-    int getAllocSize(){
-        return allocSize;
-    }
-
-    int getBlocks() {
-        return blocksNumber;
-    }
-    void reAlloc(int increment) {
-        cout<<"INCREMENT "<<increment<<endl;
-        int tempAllocSize = allocSize+increment;
-        Block<T, blockSize> *temp = new Block<T, blockSize>[tempAllocSize];
-
-        cout<<"INFO "<<size<<" : "<<allocSize<<" : "<<tempAllocSize<<endl;
-        for (int i = 0; i < allocSize; i++) {
-            cout<<"ITERATION "<<i<<endl;
-            for(int j = 0; j<blockSize;j++){
-                temp[i].items[j] = array[i].items[j];
-            }
-
-        }
-        array = temp;
-        allocSize = tempAllocSize;
-        /*for (int i = 0; i < allocSize; i++) {
-            delete temp[i];
-        }*/
-        //delete[](temp);
-        cout << "SIZE " << size <<" : AllocSize "<<allocSize<< endl;
-    }
-
-
-
 };
 
 
--- tech_prog_lab3/main.cpp
+++ tech_prog_lab3/main.cpp
@@ -13,52 +13,11 @@ public:
 };
 
 LinkedList<Item, blockSize> linkedList;
-Item item;
 
-void print() {
-    int block;
-    int item;
-    int size = linkedList.getSize();
-    cout << "SIZE " << size <<" : AllocSize "<<linkedList.getAllocSize()<< endl;
-    for (int i = 0; i < size; i++) {
-        block = i / blockSize;
-        item = i % blockSize;
-        cout << i << ": Block[" << block << "] Item[" << item << "]:\t" <<
-        linkedList.array[block].items[item].getData().key << endl;
-    }
-    cout << "SIZE " << size <<" : AllocSize "<<linkedList.getAllocSize()<< endl;
-    /*for(int i = 0; i<=linkedList.getSize(); i++){
-        for(int j = 0; j<linkedList.getBlock(i).getSize(); j++){
-            cout<<"Block["<<i<<"] Item["<<j<<"]:\t"<<linkedList.getBlock(i).get(j).getData().key<<endl;
-        }
-        cout<<endl;
-    }*/
-}
 
 int main() {
-    item.key = 0;
-    item.fieldFloat = 10;
-    item.fieldChar = 'l';
 
-    for (int i = 0; i < 7; i++) {
-        item.key += 10;
-        linkedList.add(item);
-    }
-    print();
 
-    cout << endl << endl;
-
-    item.key = 228;
-    linkedList.add(item, 9);
-    linkedList.add(item, 9);
-    item.key = 332;
-    linkedList.addBegin(item);
-    linkedList.add(item, 15);
-    print();
-    linkedList.add(item, 33);
-    //linkedList.add(item, 20480);
-
-    print();
 
     return 0;
 }
